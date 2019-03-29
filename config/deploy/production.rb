d2bd1472f068a29ccfc926f8555257569f54df3a
Bartłomiej Karaczyn <bartlomiej.karaczyn@gmail.com>
2019-03-29 20:36:09 +0100

    Changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,1 @@
-server 's34.mydevil.net', user: 'rzemyk87', roles: %w{web app db}
-set :rails_env, :production
-set :deploy_to, '/home/rzemyk87/domains/controlcrm.pl/public_ruby'
+server '138.68.104.141', user: 'deploy', roles: %w{app db web}
